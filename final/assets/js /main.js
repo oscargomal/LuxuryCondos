cdf10cddbb0b54aa94aeaf66bbcd406219ae6ec2
hoshilgordillo <131888431+hoshilgordillo@users.noreply.github.com>
2026-01-17 13:51:50 -0600

    Update main.js

--- final/assets/js /main.js
+++ final/assets/js /main.js
@@ -1,1 +1,24 @@
+/* ================= GALERÍA ================= */
+const images = document.querySelectorAll('.gallery-grid img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightboxImg');
+const closeBtn = document.querySelector('.lightbox .close');
+
+images.forEach(img => {
+  img.addEventListener('click', () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = 'flex';
+  });
+});
+
+closeBtn.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target !== lightboxImg) {
+    lightbox.style.display = 'none';
+  }
+});
+
 
